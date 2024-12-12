b7510db8430a1b69c6761c91c0c219b84010c59e
Brendan Dash <me@aiwan.run>
2024-12-12 14:33:21 +0800

    chore: use tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts", "src/worker.ts"],
   format: "esm",
+  outDir: "dist",
   sourcemap: true,
   dts: true,
   clean: true,
